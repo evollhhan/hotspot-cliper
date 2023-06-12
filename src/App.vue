59dc62f185d7c78142378085e44317a180659352
evollhhan <evollhhan@gmail.com>
2023-06-12 16:51:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="box" @click="onclick">
+  </div>
+</template>
+
+<script setup lang="ts">
+const onclick = () => {
+  console.log('clicked')
+}
+</script>
+
+<style scoped>
+.box {
+  width: 300px;
+  height: 300px;
+  background-color: #f33;
+}
+</style>
