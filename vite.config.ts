732e7319f054485b8ad46b15d6b5b289c11966c2
evollhhan <evollhhan@gmail.com>
2023-07-06 17:52:37 +0800

    fix public path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/hotspot-cliper/' : '/',
   build: {
     outDir: 'docs',
   },
